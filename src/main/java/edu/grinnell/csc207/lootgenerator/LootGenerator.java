89fb0ec0da3276a17e8a261aed03317181fe69af
Trenton Cochell <trentoncochell@gmail.com>
2025-05-14 20:02:10 -0500

    Implemented the beginnings of the program, fixed a variety of bugs.

--- src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
+++ src/main/java/edu/grinnell/csc207/lootgenerator/LootGenerator.java
@@ -1,11 +1,26 @@
 package edu.grinnell.csc207.lootgenerator;
 
+import java.io.FileNotFoundException;
+
 public class LootGenerator {
-    /** The path to the dataset (either the small or large set). */
+
+    /**
+     * The path to the dataset (either the small or large set).
+     */
     private static final String DATA_SET = "data/small";
-    
-    public static void main(String[] args) {
-        System.out.println("This program kills monsters and generates loot!");
-        // TOOD: Implement me!
+
+    public static void main(String[] args) throws FileNotFoundException, Exception {
+        //gets our monster and its treasure class
+        Monster monst = new Monster();
+        String[] monstAndTreas;
+        monstAndTreas = monst.pickMonster(DATA_SET + "/monstats.txt");
+
+        //gets an item from the provided treasure class
+        TreasureClass tc = new TreasureClass();
+        String item;
+        item = tc.fetchTreasureClass(DATA_SET + "/TreasureClassEx.txt", monstAndTreas[1]);
+
+        //this should not be printing out Cow(H) but it is...
+        System.out.println("Item is:" + item);
     }
 }
--- src/main/java/edu/grinnell/csc207/lootgenerator/Monster.java
+++ src/main/java/edu/grinnell/csc207/lootgenerator/Monster.java
@@ -11,94 +11,104 @@ import java.util.Scanner;
  * @author trent
  */
 public class Monster {
-    private String mClass;
-    private String type;
-    private int level;
-    private String TC; 
-    
-    public Monster(){
 
+    public Monster() {
     }
-    
+
     private int size(ArrayList<String> monsters) {
         return monsters.size();
     }
-    
+
     /**
-     * Creates a new arraylist and uses a scanner to parse through the file
-     * and assign each index of the Arraylist to the string representation of 
-     * each other lines of text in the monster file.
+     * Creates a new arraylist and uses a scanner to parse through the file and
+     * assign each index of the Arraylist to the string representation of each
+     * other lines of text in the monster file.
+     *
      * @param path File path
-     * @return The arraylist populated, each index being the string representation
-     * of one of the lines of text in the monster file.
+     * @return The arraylist populated, each index being the string
+     * representation of one of the lines of text in the monster file.
      * @throws FileNotFoundException
      */
-    public ArrayList<String> monsterToArrayList(File path) throws FileNotFoundException {
+    public ArrayList<String> monsterToArrayList(String path) throws FileNotFoundException {
         ArrayList<String> monsters = new ArrayList();
-        Scanner scanner = new Scanner(path);
+        Scanner scanner = new Scanner(new File(path));
         String line;
-        while(scanner.hasNextLine()){
+        while (scanner.hasNextLine()) {
             line = scanner.nextLine();
-            monsters.add(line);  //POTENTIAL PROBLEM HERE OF SKIPPING THE FIRST LINE
+            monsters.add(line);
         }
         return monsters;
     }
-    
+
     /**
      * Creates a random number no larger than the size of the arrayList and gets
      * a monster at the index being the random number that was made.
+     *
      * @param monsters an arraylist of strings that are monsters
      * @return A random monster as a string
      */
     public String monsterPicker(ArrayList<String> monsters) {
         Random random = new Random();
         int rand = random.nextInt(size(monsters));
-        return(monsters.get(rand));
+        return (monsters.get(rand));
     }
 
     /**
      * Gets the strings we want (name and treasure class) from the main string
+     *
      * @param monster
-     * @return String[] 
+     * @return String[]
      */
     public String[] vals(String monster) {
         //Declaration of variables
         String[] vals = new String[2];
         int checker = 0;
-        ArrayList<Character> monst = null;
-        ArrayList<Character> treasure = null;
-        
+        ArrayList<Character> monst = new ArrayList<>();
+        ArrayList<Character> treasure = new ArrayList<>();
+
         //Goes through whole string and grabs what we want
-        for(int i = 0; i < monster.length(); i++){
-            if(monster.charAt(i) == '\t') {
+        for (int i = 0; i < monster.length(); i++) {
+            if (monster.charAt(i) == '\t') {
                 checker++;
             }
-            if(checker == 0){
+            if (checker == 0) {
                 monst.add(monster.charAt(i));
             }
-            if(checker == 3) {
+            if (checker == 3) {
                 treasure.add(monster.charAt(i));
             }
         }
-        
+
         //Converts the ArrayLists of chars into strings
         StringBuilder sb1 = new StringBuilder();
         for (Character c : monst) {
             sb1.append(c);
         }
         String strMonst = sb1.toString();
-        
+
         StringBuilder sb2 = new StringBuilder();
         for (Character d : treasure) {
             sb2.append(d);
         }
         String strTreasure = sb2.toString();
-        
+
         //Adds those strings into our array
         vals[0] = strMonst;
         vals[1] = strTreasure;
-        
+
         //Return the array of strings of the values we care about
         return vals;
+    }
+
+    /**
+     * Calls all helper functions above to get the monster name and treasure
+     * class to return
+     *
+     * @param path
+     * @return String array of the monster name and its treasure class
+     * @throws FileNotFoundException
+     */
+    public String[] pickMonster(String path) throws FileNotFoundException {
+        return (vals(monsterPicker(monsterToArrayList(path))));
     }
 }
